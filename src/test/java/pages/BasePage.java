9654664f1f2b95ca911522039ae94af5a43d623f
RedwanParvez100 <106326119+RedwanParvez100@users.noreply.github.com>
2025-09-22 23:18:29 +0600

    added test cases : TestCarouselDisplay, TestOpenCarousel

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -1,12 +1,15 @@
 package pages;
 
-import io.qameta.allure.Allure;
+//import io.qameta.allure.Allure;
 import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.ByteArrayInputStream;
+import java.time.Duration;
 
 import static utilities.DriverSetup.getDriver;
 
@@ -48,7 +51,17 @@ public class BasePage {
         return getDriver().getCurrentUrl();
     }
 
-    public void addScreenshot(String name){
-        Allure.addAttachment(name, new ByteArrayInputStream(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES)));
+//    public void addScreenshot(String name){
+//        Allure.addAttachment(name, new ByteArrayInputStream(((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.BYTES)));
+//    }
+
+    public void waitForElementPresence(By locator) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public void waitForElementToBeClickable(By locator) {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(20));
+        wait.until(ExpectedConditions.elementToBeClickable(locator));
     }
 }
--- src/test/java/testcases/TestHomePage.java
+++ src/test/java/testcases/TestHomePage.java
@@ -1,15 +1,17 @@
 package testcases;
 
-import io.qameta.allure.Description;
+//import io.qameta.allure.Description;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 import pages.HomePage;
 import utilities.DriverSetup;
 
 public class TestHomePage extends DriverSetup {
 
     HomePage homePage = new HomePage();
+    SoftAssert softAssert = new SoftAssert();
 
     @BeforeMethod
     public void loadHomePageForTest(){
@@ -17,25 +19,45 @@ public class TestHomePage extends DriverSetup {
         homePage.clickOnElement(homePage.pop_up_btn);
     }
 
-    @Test(description = "Test Home Page Title", priority = 1, groups = "Sanity, Regression")
-    @Description ("Allure - Test Home Page Title")
+    @Test(description = "Test Home Page Title", priority = 0, groups = "Sanity, Regression")
+//    @Description ("Allure - Test Home Page Title")
     public void TestHomePageTitle(){
 //        homePage.addScreenshot("Home page");
         Assert.assertEquals(getDriver().getTitle(), homePage.title);
     }
 
-    @Test(priority = 0, description = "Verify different types of books are displayed on the homepage.")
+    @Test(priority = 1, description = "Verify different types of books are displayed on the homepage.")
+//    @Description("Test All types of books are displayed in the homepage.")
     public void TestBookTypesDisplayed(){
-        homePage.addScreenshot("Home page");
-        homePage.isVisible(homePage.book_types1);
-//        homePage.clickOnElement(homePage.login_signup_btn);
-//        Assert.assertFalse(getDriver().getCurrentUrl().equals(homePage.url));
+        Assert.assertTrue(homePage.isVisible(homePage.book_types1));
+        Assert.assertTrue(homePage.isVisible(homePage.book_types2));
+        Assert.assertTrue(homePage.isVisible(homePage.book_types3));
+        Assert.assertTrue(homePage.isVisible(homePage.book_types4));
+        Assert.assertFalse(homePage.isVisible(homePage.book_types5));
+//        homePage.addScreenshot("Home page");
     }
 
-    @Test(priority = 0, groups = "Sanity")
-    public void TestLoginSignupButton(){
-        homePage.addScreenshot("Home page");
-        homePage.clickOnElement(homePage.login_signup_btn);
-        Assert.assertFalse(getDriver().getCurrentUrl().equals(homePage.url));
+    @Test(priority = 2, description = "Verify that any deals, discounts, or carousel banner are correctly displayed on the home page.")
+//    @Description("Test Deals, discounts, or promotions should be displayed correctly.")
+    public void TestCarouselDisplay(){
+//        getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        Assert.assertTrue(homePage.isVisible(homePage.carousel_3));
+        softAssert.assertTrue(homePage.isVisible(homePage.carousel_4));
     }
+
+    @Test(priority = 3, description = "Verify that carousel banners are working perfectly.")
+//    @Description("Should open a new page or same page after clicking the banner.")
+    public void TestOpenCarousel(){
+        homePage.clickOnElement(homePage.carousel_4th_bottom_icon);
+        homePage.clickOnElement(homePage.carousel_4);
+        Assert.assertTrue(homePage.isVisible(homePage.carousel_4th_related_all_books));
+        Assert.assertFalse(getDriver().getCurrentUrl().equals(homePage.url));
+        getDriver().navigate().back();
+        Assert.assertTrue(getDriver().getCurrentUrl().equals(homePage.url));
+        homePage.clickOnElement(homePage.carousel_right_side_option);
+        Assert.assertTrue(homePage.isVisible(homePage.carousel_2));
+        homePage.clickOnElement(homePage.carousel_left_side_option);
+        Assert.assertTrue(homePage.isVisible(homePage.carousel_1));
+    }
+
 }
